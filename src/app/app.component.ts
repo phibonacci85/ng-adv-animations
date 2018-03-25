67db1cc2eb2d8675b88ac7ac5c705934ab76b3de
John Baker <phibonacci85@gmail.com>
2018-03-25 16:17:19 -0500

    advanced animations finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
+import { animateStateTrigger, listStateTrigger, showStateTrigger } from './animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    showStateTrigger,
+    animateStateTrigger,
+    listStateTrigger
+  ]
 })
 export class AppComponent {
-  title = 'app works!';
+  isShown = false;
+  width = 400;
+  animate = false;
+  testResults = [];
+
+  onAddElement() {
+    this.testResults.push(Math.random());
+  }
+
+  onAnimationStart(event: AnimationEvent) {
+    console.log(event);
+  }
+
+  onAnimationEnd(event: AnimationEvent) {
+    console.log(event);
+  }
 }
